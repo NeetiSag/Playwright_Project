4c78988f5a8a25b2312c59f632211eb839dfb9bf
NeetiSag <neetis282@gmail.com>
2025-04-08 00:42:17 -0700

    Fetching All Products

--- tests/UIBasictstest.spec.js
+++ tests/UIBasictstest.spec.js
@@ -1,19 +1,36 @@
 const {test,expect} = require('@playwright/test')
 
 
-test('First Playwright test',async ({browser})=>
+test('@Web Browser Context-Validating Error Login',async ({browser})=>
 {
+    
     const context = await browser.newContext();
 
     const page = await context.newPage();
-
+    const userName = page.locator("#username")
+    const signIn = page.locator("#signInBtn")
+    const cardTitles = page.locator(".card-body a")
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
+    await userName.fill("rahulshetty");
+    await page.locator("[type='password']").fill("learning");
+    await signIn.click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect')
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+    console.log(await cardTitles.first().textContent());
+    console.log(await cardTitles.nth(1).textContent());
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
 
 
 });
 
 
+
+/*
 test.only ('Page Playwright test',async ({page})=>
 {
 
@@ -21,3 +38,4 @@ await page.goto("https://google.com");
 console.log(await page.title());
 await expect(page).toHaveTitle("Google");
 });
+*/
